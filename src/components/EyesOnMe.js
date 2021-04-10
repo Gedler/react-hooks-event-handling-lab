c1146d2de1270e0eb67501feff8a15aafe01287c
Gedler <gedler.jeanpierre@stonybrook.edu>
2021-04-10 11:54:16 -0400

    Completed: Using Event Handlers with Callbacks

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+//In the components/EyesOnMe.js file, create a EyesOnMe React component.
+// In that component, render a button with the text 'Eyes on me'.
+// On that button, add event handlers that listens for the focus and blur events.
+// When the focus event fires, use console.log to print out the text 'Good!'.
+// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
+
+function EyesOnMe() {
+    console.log("All eyes on me")
+    return (
+        <button onFocus={focus} onBlur={blur}>
+            Hey! Eyes on me! 
+            </button>
+    )
+}
+
+function focus () {
+    console.log("Good!")
+}
+
+function blur () {
+    console.log("Hey! Eyes on me!")
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+function KeyPad (){
+    console.log("I am the keypad!")
+    return (
+    <input type="password" name="keypad" onChange={handleChange}/>
+    )}
+
+function handleChange() {
+    console.log("Entering password...")
+}
+    
+export default KeyPad; 
+
